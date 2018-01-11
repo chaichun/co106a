e59e70815b3f8038254e00fcbe21857ae05d2c38
chaichun <k2159786@yahoo.com.tw>
2018-01-11 09:36:29 +0800

    update

--- exercise/04/mult/mult.asm
+++ exercise/04/mult/mult.asm
@@ -7,3 +7,27 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+ //R2 = 0
+ @0
+ D=A
+ @R2
+ M=D
+//R0 >0 的時候才會繼續執行
+(LOOP)
+    @R0
+    D=M
+    @END
+    D;JLE
+// R2=R2+R1
+    @R1
+    D=M
+    @R2
+    M=M+D
+//R1 =R1-1
+    @R1
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP  
